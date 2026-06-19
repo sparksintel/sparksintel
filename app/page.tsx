0e01b84b0cd84a55b7b32376b3f5a0ebb2f4dea3
Rajeshwar Burla <admin@sparksintel.com>
2026-06-19 07:43:04 +0530

    fix hoveredCard type in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [hoveredCard, setHoveredCard] = useState(null);
+  const [hoveredCard, setHoveredCard] = useState<number | null>(null);
 
   const propositions = [
     {
